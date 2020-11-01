b458bb8a013ecb50e44448504a4b3ba01f4d4c08
Antuan01 <joseitox_4@hotmail.com>
2020-10-31 23:34:44 -0400

    sql working

--- sqlModels.py
+++ sqlModels.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey, Table
+from sqlalchemy import Boolean, DateTime, Column, Integer, String, ForeignKey, Table, func
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy
